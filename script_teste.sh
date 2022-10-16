1d9c039260ffe2c8b470adf2fc3e9d87809e71c1
Bruno Melo <43672635+maxcalavera81@users.noreply.github.com>
2022-10-17 00:16:14 +0100

    Update script_teste.sh

--- script_teste.sh
+++ script_teste.sh
@@ -5,22 +5,52 @@
 ##                                                                   ##
 #######################################################################
 #######################################################################
-install_requirements() {
-  echo ""
-  echo "Instalar Armbian Software..."
-  echo ""
-  armbian-software
+set -o errexit  # Exit script when a command exits with non-zero status
+set -o errtrace # Exit on error inside any functions or sub-shells
+set -o nounset  # Exit script on use of an undefined variable
+set -o pipefail # Return exit status of the last command in the pipe that failed
+
+# ==============================================================================
+# GLOBALS
+# ==============================================================================
+readonly HOSTNAME="homeassistant"
+
+# ==============================================================================
+# SCRIPT LOGIC
+# ==============================================================================
+
+# ------------------------------------------------------------------------------
+# Ensures the hostname of the Pi is correct.
+# ------------------------------------------------------------------------------
+update_hostname() {
+  old_hostname=$(< /etc/hostname)
+  if [[ "${old_hostname}" != "${HOSTNAME}" ]]; then
+    sed -i "s/${old_hostname}/${HOSTNAME}/g" /etc/hostname
+    sed -i "s/${old_hostname}/${HOSTNAME}/g" /etc/hosts
+    hostname "${HOSTNAME}"
+    echo ""
+    echo "O nome do host será alterado na próxima reinicialização: ${HOSTNAME}"
+    echo ""
+  fi
 }
- 
 
 # ------------------------------------------------------------------------------
 # Installs all required software packages and tools
 # ------------------------------------------------------------------------------
-install_requirements() {
+install_armbian-software() {
   echo ""
-  echo "Atualizando APT packages list..."
+  echo "A instalar Armbian Software..."
   echo ""
+  armbian-software
+}
 
+# ------------------------------------------------------------------------------
+# Installs the Docker engine
+# ------------------------------------------------------------------------------
+install_dependences() {
+  echo ""
+  echo "A instalar independencias..."
+  echo ""
   sudo apt-get install \
   apparmor \
   jq \
@@ -40,15 +70,15 @@ install_docker() {
   echo ""
   echo "A instalar Docker..."
   echo ""
-  curl -fsSL get.docker.com | sh
+  curl -fsSL https://get.docker.com | sh
 }
 
 # ------------------------------------------------------------------------------
-# Installs os agents
+# Install os-agents
 # ------------------------------------------------------------------------------
-install_os_agents() {
+install_osagents() {
   echo ""
-  echo "A instalar o Home Assistant..."
+  echo "A instalar os agents..."
   echo ""
   wget https://github.com/home-assistant/os-agent/releases/download/1.4.1/os-agent_1.4.1_linux_aarch64.deb
   sudo dpkg -i os-agent_1.4.1_linux_aarch64.deb
@@ -56,16 +86,38 @@ install_os_agents() {
 }
 
 # ------------------------------------------------------------------------------
-# Install Home Assistant Supervisor
+# Installs and starts Hass.io
 # ------------------------------------------------------------------------------
-install_os_agents() {
+install_hassio() {
   echo ""
   echo "A instalar o Home Assistant..."
   echo ""
+  apt-get update
+  apt-get install udisks2 wget -y
   wget https://github.com/home-assistant/supervised-installer/releases/latest/download/homeassistant-supervised.deb
-  sudo dpkg -i os-agent_1.4.1_linux_aarch64.deb
   sudo dpkg -i homeassistant-supervised.deb
 }
+
+# ==============================================================================
+# RUN LOGIC
+# ------------------------------------------------------------------------------
+main() {
+  # Are we root?
+  if [[ $EUID -ne 0 ]]; then
+    echo "This script must be run as root."
+    echo "Please try again after running:"
+    echo "  sudo su"
+    exit 1
+  fi
+
+  # Install ALL THE THINGS!
+  update_hostname
+  install_armbian-software
+  install_dependences
+  install_docker
+  install_osagents
+  install_hassio
+  upgrade_final
 
   # Friendly closing message
   ip_addr=$(hostname -I | cut -d ' ' -f1)
