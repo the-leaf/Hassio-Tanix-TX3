20416860b6b58381578c191e1b362b7b72fded95
Bruno Melo <43672635+maxcalavera81@users.noreply.github.com>
2022-10-18 00:46:46 +0100

    Update script_teste.sh

--- script_teste.sh
+++ script_teste.sh
@@ -41,7 +41,7 @@ install_armbian-software() {
   echo ""
   echo "A instalar Armbian Software..."
   echo ""
-  (echo "108" && cat) | armbian-software
+  echo -ne '\n' armbian-sofware
   #armbian-software
 }
 
