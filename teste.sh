5b1b85a1f4b339864c7d53dfb58d3ee203c73a62
Bruno Melo <43672635+maxcalavera81@users.noreply.github.com>
2022-10-17 23:35:03 +0100

    Update teste.sh

--- teste.sh
+++ teste.sh
@@ -15,6 +15,25 @@ set -o pipefail # Return exit status of the last command in the pipe that failed
 # ==============================================================================
 readonly HOSTNAME="homeassistant"
 
+# ==============================================================================
+# SCRIPT LOGIC
+# ==============================================================================
+
+# ------------------------------------------------------------------------------
+# Ensures the hostname of the Pi is correct.
+# ------------------------------------------------------------------------------
+update_hostname() {
+  old_hostname=$(< /etc/hostname)
+  if [[ "${old_hostname}" != "${HOSTNAME}" ]]; then
+    sed -i "s/${old_hostname}/${HOSTNAME}/g" /etc/hostname
+    sed -i "s/${old_hostname}/${HOSTNAME}/g" /etc/hosts
+    hostname "${HOSTNAME}"
+    echo ""
+    echo "O nome do host será alterado na próxima reinicialização: ${HOSTNAME}"
+    echo ""
+  fi
+}
+
 # ------------------------------------------------------------------------------
 # Installs all required software packages and tools
 # ------------------------------------------------------------------------------
@@ -25,17 +44,91 @@ install_armbian-software() {
   armbian-software
 }
 
-install_home_assistant() {
-  echo "This script is about to run another script."
-  curl -sL https://raw.githubusercontent.com/maxcalavera81/Hassio-Tanix-TX3/main/script_teste.sh | bash -s
-  echo "This script has just run another script."
+
+# ------------------------------------------------------------------------------
+# Installs the Docker engine
+# ------------------------------------------------------------------------------
+install_dependences() {
+  echo ""
+  echo "A instalar independencias..."
+  echo ""
+  sudo apt-get install \
+  apparmor \
+  jq \
+  wget \
+  curl \
+  udisks2 \
+  libglib2.0-bin \
+  network-manager \
+  dbus \
+  systemd-journal-remote -y
 }
+
+# ------------------------------------------------------------------------------
+# Installs the Docker engine
+# ------------------------------------------------------------------------------
+install_docker() {
+  echo ""
+  echo "A instalar Docker..."
+  echo ""
+  curl -fsSL https://get.docker.com | sh
+}
+
+# ------------------------------------------------------------------------------
+# Install os-agents
+# ------------------------------------------------------------------------------
+install_osagents() {
+  echo ""
+  echo "A instalar os agents..."
+  echo ""
+  wget https://github.com/home-assistant/os-agent/releases/download/1.4.1/os-agent_1.4.1_linux_aarch64.deb
+  sudo dpkg -i os-agent_1.4.1_linux_aarch64.deb
+  systemctl status haos-agent
+}
+
+# ------------------------------------------------------------------------------
+# Installs and starts Hass.io
+# ------------------------------------------------------------------------------
+install_hassio() {
+  echo ""
+  echo "A instalar o Home Assistant..."
+  echo ""
+  apt-get update
+  apt-get install udisks2 wget -y
+  wget https://github.com/home-assistant/supervised-installer/releases/latest/download/homeassistant-supervised.deb
+  sudo dpkg -i homeassistant-supervised.deb
+}
+
+# ==============================================================================
+# RUN LOGIC
+# ------------------------------------------------------------------------------
+main() {
+  # Are we root?
+  if [[ $EUID -ne 0 ]]; then
+    echo "This script must be run as root."
+    echo "Please try again after running:"
+    echo "  sudo su"
+    exit 1
+  fi
 
   # Install ALL THE THINGS!
 #  update_hostname
   install_armbian-software
-#  install_home_assistant
 #  install_dependences
 #  install_docker
 #  install_osagents
 #  install_hassio
+
+  # Friendly closing message
+  ip_addr=$(hostname -I | cut -d ' ' -f1)
+  echo "======================================================================="
+  echo "Hass.io está agora a instalar o Home Assistant."
+  echo "Este processo demora a volta de  20 minutes. Abre o seguinte link:"
+  echo "http://${HOSTNAME}.local:8123/ no teu browser"
+  echo "para carregar o home assistant."
+  echo "Se o link acima não funcionar, tenta o seguinte link http://${ip_addr}:8123/"
+  echo "Aproveita o teu home assistant :)"
+
+  exit 0
+}
+main
