27278b12859008f8a1e33cc83136a856a2f4a3df
Bruno Melo <43672635+maxcalavera81@users.noreply.github.com>
2022-10-17 23:31:12 +0100

    Update teste.sh

--- teste.sh
+++ teste.sh
@@ -31,18 +31,6 @@ install_home_assistant() {
   echo "This script has just run another script."
 }
 
-# ==============================================================================
-# RUN LOGIC
-# ------------------------------------------------------------------------------
-main() {
-  # Are we root?
-  if [[ $EUID -ne 0 ]]; then
-    echo "This script must be run as root."
-    echo "Please try again after running:"
-    echo "  sudo su"
-    exit 1
-  fi
-
   # Install ALL THE THINGS!
 #  update_hostname
   install_armbian-software
@@ -52,16 +40,4 @@ main() {
 #  install_osagents
 #  install_hassio
 
-  # Friendly closing message
-  ip_addr=$(hostname -I | cut -d ' ' -f1)
-  echo "======================================================================="
-  echo "Hass.io está agora a instalar o Home Assistant."
-  echo "Este processo demora a volta de  20 minutes. Abre o seguinte link:"
-  echo "http://${HOSTNAME}.local:8123/ no teu browser"
-  echo "para carregar o home assistant."
-  echo "Se o link acima não funcionar, tenta o seguinte link http://${ip_addr}:8123/"
-  echo "Aproveita o teu home assistant :)"
-
-  exit 0
-}
 main
